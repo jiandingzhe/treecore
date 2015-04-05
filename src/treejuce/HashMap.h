8f2394492147beac5512a2a8bf737b4d88f72d7c
Xi Yang <yangx@im.ac.cn>
2015-04-05 10:05:46 +0800

    Separate hash function wrapping from HashMap.h, allow it being used by other hashed containers.

--- src/treejuce/HashMap.h
+++ src/treejuce/HashMap.h
@@ -30,31 +30,11 @@
 #define JUCE_HASHMAP_H_INCLUDED
 
 #include "treejuce/Array.h"
+#include "treejuce/HashFunctions.h"
 #include "treejuce/LeakedObjectDetector.h"
 #include "treejuce/ScopedPointer.h"
-#include "treejuce/String.h"
-#include "treejuce/Variant.h"
 
-//==============================================================================
 TREEFACE_JUCE_NAMESPACE_BEGIN
-
-/**
-    A simple class to generate hash functions for some primitive types, intended for
-    use with the HashMap class.
-    @see HashMap
-*/
-struct DefaultHashFunctions
-{
-    /** Generates a simple hash from an integer. */
-    int generateHash (const int key, const int upperLimit) const NOEXCEPT        { return std::abs (key) % upperLimit; }
-    /** Generates a simple hash from an int64. */
-    int generateHash (const int64 key, const int upperLimit) const NOEXCEPT      { return std::abs ((int) key) % upperLimit; }
-    /** Generates a simple hash from a string. */
-    int generateHash (const String& key, const int upperLimit) const NOEXCEPT    { return (int) (((uint32) key.hashCode()) % (uint32) upperLimit); }
-    /** Generates a simple hash from a variant. */
-    int generateHash (const var& key, const int upperLimit) const NOEXCEPT       { return generateHash (key.toString(), upperLimit); }
-};
-
 
 //==============================================================================
 /**
@@ -187,8 +167,10 @@ public:
         const ScopedLockType sl (getLock());
 
         for (const HashEntry* entry = hashSlots.getUnchecked (generateHashFor (keyToLookFor)); entry != nullptr; entry = entry->nextEntry)
+        {
             if (entry->key == keyToLookFor)
                 return true;
+        }
 
         return false;
     }
