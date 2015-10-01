180375520ecc1721b23992bc3d5b821b0f3748f9
Xi Yang <jiandingzhe@163.com>
2015-10-01 13:18:21 +0800

    object pool looks good

--- src/treecore/ObjectPool.h
+++ src/treecore/ObjectPool.h
@@ -3,165 +3,165 @@
 
 #include "treecore/AlignedMalloc.h"
 #include "treecore/IntTypes.h"
-#include "treecore/LFQueue.h"
-#include "treecore/Queue.h"
 #include "treecore/LeakedObjectDetector.h"
+#include "treecore/LFQueue.h"
+#include "treecore/MPL.h"
+#include "treecore/Queue.h"
+
+class TestFramework;
 
 namespace treecore
 {
 
-template<typename T, bool MULTI_THREAD = true, int BLOCK_SIZE = 4*1024>
+/**
+ * @brief hold memory cache for fast creation of many objects
+ *
+ */
+template<typename T, bool MULTI_THREAD = true, int BLOCK_SIZE = 4096>
 class ObjectPool
 {
-    class ObjBlock: public AlignedMalloc<AlignOf<T>::value>
-    {
-    public:
-        ObjBlock();
-        ~ObjBlock();
-        RawType* operator[]( const int i );
+    friend class ::TestFramework;
 
-    private:
-        int8 m_elements[sizeof(T) * BLOCK_SIZE];
-        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ObjBlock);
-    };
+    TREECORE_ALN_BEGIN(alignof(T)) struct ObjBlock
+    {
+        ObjBlock() {}
+        ~ObjBlock() {}
+
+        T* operator[]( const int i )
+        {
+            return (T*) &m_data[i*sizeof(T)];
+        }
+
+        int8 m_data[sizeof(T) * BLOCK_SIZE];
+
+        TREECORE_ALIGNED_ALLOCATOR(ObjBlock);
+        TREECORE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ObjBlock);
+    } TREECORE_ALN_END(alignof(T));
 
     typedef typename mpl_type_if<MULTI_THREAD, LfQueue<ObjBlock*> , Queue<ObjBlock*>>::type BlockQueueType;
     typedef typename mpl_type_if<MULTI_THREAD, LfQueue<T*>, Queue<T*>>::type ValueQueueType;
 
 public:
-    ObjectPool(int preAllocBlkNum=1);
+    typedef T ValueType;
 
     /**
-     * After released, elements created from this pool became invalid
+     * @brief create object pool
+     * @param num_blocks_init number of initially built blocks
      */
-    ~ObjectPool();
+    ObjectPool(int num_blocks_init = 1)
+        : m_blocks(11) //2^11个,够了
+        , m_objects(17) //2^17个空位置,应该够了吧
+    {
+        createSome(num_blocks_init);
+    }
 
-    T* Pop();
+    /**
+     * @brief After released, elements created from this pool became invalid
+     */
+    ~ObjectPool()
+    {
+        ObjBlock* k;
+        likely_while(m_blocks.pop(k)) { delete k; }
+    }
 
+    /**
+     * @brief get one object which is built by its default constructor
+     * @return object of type T
+     */
+    T* generate()
+    {
+        T* k = nullptr;
+        unlikely_if(!m_objects.pop(k))
+        {
+            createSome();
+        }
+
+        new (k) T();
+        return k;
+    }
+
+
+    /**
+     * @brief get one object with specified constructor parameters
+     * @return object of type T
+     */
     template<typename... InitType>
-    T* Pop(InitType&... initValues);
+    T* generate(InitType... initValues) // TODO ref type or value type?
+    {
+        T* k = nullptr;
+        unlikely_if(!m_objects.pop(k))
+        {
+            createSome();
+        }
 
-    void Push(T* k);
+        new (k) T(initValues...);
+        return k;
+    }
 
-    //* 手动创建一些新元素,并加入池中
-    void CreateSome(int allocBlkNum = 1);
+    /**
+     * @brief recycle this object
+     * @param k object to recycle
+     */
+    void recycle(T* k)
+    {
+        unlikely_if(k == nullptr) return;
+        k->~T();
+        m_objects.push(k);
+    }
+
+    /**
+     * @brief create some additional memory blocks
+     * @param numBlock number of memory blocks to create
+     */
+    void createSome(int numBlock = 1)
+    {
+        jassert( numBlock>0 );
+
+
+        for (int i = 0; i < numBlock; i++)
+        {
+            ObjBlock*const blk = new ObjBlock();
+            m_blocks.push(blk);
+
+            for (int j = 0; j < BLOCK_SIZE; ++j) {
+                m_objects.push( (*blk)[j] );
+            }
+        }
+    }
 
 private:
     BlockQueueType m_blocks;
-    ValueQueueType m_reuseBuffer;
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ObjectPool);
+    ValueQueueType m_objects;
+    TREECORE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ObjectPool);
 };
 
-//==================================================================================================
 
-template<typename T, bool MULTI_THREAD, int BLOCK_SIZE>
-ObjectPool<T, MULTI_THREAD, BLOCK_SIZE>::ObjectPool(int preAllocBlkNum)
-    : m_blocks(11) //2^11个,够了
-    , m_reuseBuffer(17) //2^17个空位置,应该够了吧
-{
-    //static_assert( IS_TRIVIAL( T ) , "T must be pod!!!" );
-    CreateSome(preAllocBlkNum);
-}
-
-template<typename T,bool MULTI_THREAD,int BLOCK_SIZE>
-ObjectPool<T,MULTI_THREAD,BLOCK_SIZE>::~ObjectPool()
-{
-    ObjBlock* k;
-    likely_while(m_blocks.pop(k)) { delete k; }
-}
-
-template<typename T,bool MULTI_THREAD,int BLOCK_SIZE>
-template<typename... InitType>
-T* ObjectPool<T,MULTI_THREAD,BLOCK_SIZE>::Pop(InitType&... initValues)
-{
-    RawType* k;
-    unlikely_while(!m_reuseBuffer.pop(k)) {
-        CreateSome();
-    }
-    T* x = new(k)T(initValues...);
-    return x;
-}
-
-template<typename T,bool MULTI_THREAD,int BLOCK_SIZE>
-T* ObjectPool<T,MULTI_THREAD,BLOCK_SIZE>::Pop()
-{
-    RawType* k;
-    unlikely_while(!m_reuseBuffer.pop(k)) {
-        CreateSome();
-    }
-    T* x= new(k) T();
-    return x;
-}
-
-
-template<typename T,bool MULTI_THREAD,int BLOCK_SIZE>
-void ObjectPool<T,MULTI_THREAD,BLOCK_SIZE>::Push(T* k)
-{
-    unlikely_if(k == nullptr) return;
-    k->~T();
-    m_reuseBuffer.push( (RawType*)(k) );
-    // 这里应该没问题,为什么呢? 因为k必须是T类型的,而T类型肯定是从这个pool里面放出去的才对,push回来的不可能是个子类,
-    // 即使是子类,也已经在push的时候转回T了
-}
-
-
-template<typename T,bool MULTI_THREAD,int BLOCK_SIZE>
-void ObjectPool<T,MULTI_THREAD,BLOCK_SIZE>::CreateSome(int allocBlkNum)
-{
-    jassert( allocBlkNum>0 );
-    int i=0;
-    do {
-        ObjBlock*const m = new ObjBlock;
-        m_blocks.push(m);
-        for(int i = 0; i < BLOCK_SIZE; ++i) {
-            m_reuseBuffer.push( (*m)[i] );
-        }
-    } unlikely_while( ++i < allocBlkNum );
-}
-
-template<typename T,bool MULTI_THREAD,int BLOCK_SIZE>
-typename ObjectPool<T , MULTI_THREAD , BLOCK_SIZE>::RawType* ObjectPool<T , MULTI_THREAD , BLOCK_SIZE>::ObjBlock::operator[]( const int i )
-{
-    return &m_elements[i];
-}
-
-template<typename T,bool MULTI_THREAD,int BLOCK_SIZE>
-ObjectPool<T,MULTI_THREAD,BLOCK_SIZE>::ObjBlock::ObjBlock()
-{}
-
-
-template<typename T,bool MULTI_THREAD,int BLOCK_SIZE>
-ObjectPool<T,MULTI_THREAD,BLOCK_SIZE>::ObjBlock::~ObjBlock()
-{}
-
-//============================================================================================
-
-template<typename T , bool MULTI_THREAD = true , int allocNum = 4 * 1024>
-class CreateFromPool : public SharedSingleton< ObjectPool<T , MULTI_THREAD , allocNum> >::EarlyUser
-{
-private:
-    typedef SharedSingleton< ObjectPool<T , MULTI_THREAD , allocNum> > CreatePool;
-public:
-    static forcedinline void CreateSome(int blkNum=1)
-    {
-        CreatePool::getInstance().CreateSome(blkNum);
-    }
-    static forcedinline T* alloc()
-    {
-        return CreatePool::getInstance().Pop();
-    };
-    template<typename... InitType>
-    static forcedinline T* alloc( InitType&... initTypes )
-    {
-        return CreatePool::getInstance().Pop( initTypes... );
-    };
-    static forcedinline void free( T* ptr )
-    {
-        return CreatePool::getInstance().Push(ptr);
-    };
-protected:
-    TRIVIAL_CLASS(CreateFromPool);
-};
+//template<typename T , bool MULTI_THREAD = true , int allocNum = 4 * 1024>
+//class CreateFromPool : public SharedSingleton< ObjectPool<T , MULTI_THREAD , allocNum> >::EarlyUser
+//{
+//private:
+//    typedef SharedSingleton< ObjectPool<T , MULTI_THREAD , allocNum> > CreatePool;
+//public:
+//    static forcedinline void CreateSome(int blkNum=1)
+//    {
+//        CreatePool::getInstance().CreateSome(blkNum);
+//    }
+//    static forcedinline T* alloc()
+//    {
+//        return CreatePool::getInstance().Pop();
+//    };
+//    template<typename... InitType>
+//    static forcedinline T* alloc( InitType&... initTypes )
+//    {
+//        return CreatePool::getInstance().Pop( initTypes... );
+//    };
+//    static forcedinline void free( T* ptr )
+//    {
+//        return CreatePool::getInstance().Push(ptr);
+//    };
+//protected:
+//    TRIVIAL_CLASS(CreateFromPool);
+//};
 
 } // namespace treecore
 
--- t/t_obj_pool.cpp
+++ t/t_obj_pool.cpp
@@ -1,8 +1,98 @@
 #include "TestFramework.h"
-//#include "treecore/ObjectPool.h"
 
+#include "treecore/Array.h"
+#include "treecore/ObjectPool.h"
+
+
+struct Foo
+{
+    Foo(bool* flag): a(0), b(0.0f), flag(flag)
+    {
+        *flag = true;
+    }
+
+    Foo(int a, float b, bool* flag): a(a), b(b), flag(flag)
+    {
+        *flag = true;
+    }
+
+    ~Foo()
+    {
+        *flag = false;
+    }
+
+    treecore::int32 a;
+    float b;
+    bool* flag;
+};
 
 void TestFramework::content()
 {
+    // tests for non-threaded queue
+    {
+        bool flags[20] = {};
+        treecore::ObjectPool<Foo, false, 128> pool;
+        treecore::Array<Foo*> objs;
 
+        for (int i = 0; i < 10; i++)
+        {
+            Foo* obj = pool.generate(&flags[i]);
+            IS(obj->a, 0);
+            IS(obj->b, 0.0);
+            IS(obj->flag, &flags[i]);
+            OK(flags[i]);
+            objs.add(obj);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            Foo* obj = pool.generate(i, 12.3f, &flags[i+10]);
+            IS(obj->a, i);
+            IS(obj->b, 12.3f);
+            IS(obj->flag, &flags[i+10]);
+            OK(flags[i+10]);
+            objs.add(obj);
+        }
+
+        for (int i = 0; i < objs.size(); i++)
+        {
+            pool.recycle(objs[i]);
+            OK(!flags[i]);
+        }
+
+    }
+
+    // tests for lock-free queue
+    {
+        bool flags[20] = {};
+        treecore::ObjectPool<Foo, true, 128> pool;
+        treecore::Array<Foo*> objs;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Foo* obj = pool.generate(&flags[i]);
+            IS(obj->a, 0);
+            IS(obj->b, 0.0);
+            IS(obj->flag, &flags[i]);
+            OK(flags[i]);
+            objs.add(obj);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            Foo* obj = pool.generate(i, 12.3f, &flags[i+10]);
+            IS(obj->a, i);
+            IS(obj->b, 12.3f);
+            IS(obj->flag, &flags[i+10]);
+            OK(flags[i+10]);
+            objs.add(obj);
+        }
+
+        for (int i = 0; i < objs.size(); i++)
+        {
+            pool.recycle(objs[i]);
+            OK(!flags[i]);
+        }
+
+    }
 }
