67b601a5f275418acc9614a8177df47a03b9b00a
Xi Yang <jiandingzhe@163.com>
2015-11-27 18:01:59 +0800

    modified the way to find treecore subproject inc

--- cmake/TreeCoreInternalConfig.cmake
+++ cmake/TreeCoreInternalConfig.cmake
@@ -1,9 +1,12 @@
+set(TREECORE_INCLUDE_DIR
+    ${treecore_SOURCE_DIR}/src 
+    ${treecore_BINARY_DIR}/src 
+    CACHE STRING ""
+)
+
 function(target_use_treecore target_name)
     target_link_libraries(${target_name} treecore)
-    target_include_directories(${target_name} PRIVATE
-        ${treecore_SOURCE_DIR}/src
-        ${treecore_BINARY_DIR}/src
-    )
+    target_include_directories(${target_name} PRIVATE ${TREECORE_INCLUDE_DIR})
     treecore_set_dep_libraries(${target_name})
     treecore_set_compiler_options(${target_name})
     treecore_set_compiler_definitions(${target_name})
