ed3ebaabd094da230ff659c578eea58b733b6232
Xi Yang <jiandingzhe@163.com>
2015-11-27 17:33:29 +0800

    make treecore can be used as cmake subproject

--- cmake/FindTreeCore.cmake
+++ cmake/FindTreeCore.cmake
@@ -85,7 +85,7 @@ endfunction()
 
 function(target_use_treecore target_name)
     target_link_libraries(${target_name} treecore)
-    target_include_directories(${target_name} PUBLIC ${TREECORE_INCLUDE_DIR})
+    target_include_directories(${target_name} PRIVATE ${TREECORE_INCLUDE_DIR})
     treecore_set_dep_libraries(${target_name})
     treecore_set_compiler_options(${target_name})
     treecore_set_compiler_definitions(${target_name})
--- cmake/TreeCoreInternalConfig.cmake
+++ cmake/TreeCoreInternalConfig.cmake
@@ -1,5 +1,9 @@
 function(target_use_treecore target_name)
     target_link_libraries(${target_name} treecore)
+    target_include_directories(${target_name} PRIVATE
+        ${treecore_SOURCE_DIR}/src
+        ${treecore_BINARY_DIR}/src
+    )
     treecore_set_dep_libraries(${target_name})
     treecore_set_compiler_options(${target_name})
     treecore_set_compiler_definitions(${target_name})
