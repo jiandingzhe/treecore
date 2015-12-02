9fe71a53c1f11ac68f0748312f266a4d637dfb02
Xi Yang <jiandingzhe@163.com>
2015-12-02 20:45:22 +0800

    all-one mask of different size

--- src/treecore/IntUtils.h
+++ src/treecore/IntUtils.h
@@ -9,7 +9,7 @@ namespace treecore
 template<int SZ>
 struct sized_int;
 
-template<> struct sized_int<1> { typedef int8 type; };
+template<> struct sized_int<1> { typedef int8  type; };
 template<> struct sized_int<2> { typedef int16 type; };
 template<> struct sized_int<4> { typedef int32 type; };
 template<> struct sized_int<8> { typedef int64 type; };
@@ -17,10 +17,19 @@ template<> struct sized_int<8> { typedef int64 type; };
 template<int SZ>
 struct sized_uint;
 
-template<> struct sized_uint<1> { typedef uint8 type; };
+template<> struct sized_uint<1> { typedef uint8  type; };
 template<> struct sized_uint<2> { typedef uint16 type; };
 template<> struct sized_uint<4> { typedef uint32 type; };
 template<> struct sized_uint<8> { typedef uint64 type; };
+
+
+template<int SZ>
+struct sized_ones_mask;
+
+template<> struct sized_ones_mask<1> { static const uint8  value = 0xff; };
+template<> struct sized_ones_mask<2> { static const uint16 value = 0xffff; };
+template<> struct sized_ones_mask<4> { static const uint32 value = 0xffffffff; };
+template<> struct sized_ones_mask<8> { static const uint64 value = 0xffffffffffffffff; };
 
 template<typename T>
 struct similar_int
