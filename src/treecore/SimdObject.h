5de80c75ba671ff3fbb973e675139249b6047d0b
Xi Yang <jiandingzhe@163.com>
2016-06-06 15:03:18 +0800

    tested SimdObject comparison operators

--- src/treecore/SimdObject.h
+++ src/treecore/SimdObject.h
@@ -3,6 +3,7 @@
 
 #include "treecore/SimdFunc.h"
 #include "treecore/AlignedMalloc.h"
+#include "treecore/IntUtils.h"
 
 namespace treecore
 {
@@ -10,228 +11,297 @@ namespace treecore
 template<typename T, int N_ELEM>
 struct SimdObject
 {
-    typedef SIMDType<sizeof(T) * N_ELEM> DataType;
+    typedef SIMDType<sizeof(T)* N_ELEM> DataType;
+    typedef typename similar_int<T>::type IntType;
 
-    TREECORE_ALIGNED_ALLOCATOR(SimdObject);
+    TREECORE_ALIGNED_ALLOCATOR( SimdObject )
 
     SimdObject() noexcept
     {
-        simd_set_all<T>(data, 0, 0, 0, 0);
+        simd_set_all<T>( data, 0, 0, 0, 0 );
     }
 
-    SimdObject(const SimdObject& peer) noexcept
-        : data(peer.data)
+    SimdObject( const SimdObject& peer ) noexcept
+        : data( peer.data )
+    {}
+
+    explicit SimdObject( const DataType& value ) noexcept
+        : data( value )
+    {}
+
+    explicit SimdObject( T value ) noexcept
     {
+        simd_broadcast<T>( data, value );
     }
 
-    explicit SimdObject(const DataType& value) noexcept
-        : data(value)
+    SimdObject( const T* value_ptr ) noexcept
     {
+        simd_set_all( data, value_ptr );
     }
 
-    explicit SimdObject(T value) noexcept
+    SimdObject( T v1, T v2 ) noexcept
     {
-        simd_broadcast<T>(data, value);
+        simd_set_all<T>( data, v1, v2 );
     }
 
-    SimdObject(T v1, T v2) noexcept
+    SimdObject( T v1, T v2, T v3, T v4 ) noexcept
     {
-        simd_set_all<T>(data, v1, v2);
+        simd_set_all<T>( data, v1, v2, v3, v4 );
     }
 
-    SimdObject(T v1, T v2, T v3, T v4) noexcept
+    SimdObject( T v1, T v2, T v3, T v4, T v5, T v6, T v7, T v8 ) noexcept
     {
-        simd_set_all<T>(data, v1, v2, v3, v4);
+        simd_set_all<T>( data, v1, v2, v3, v4, v5, v6, v7, v8 );
     }
 
-    SimdObject(T v1, T v2, T v3, T v4, T v5, T v6, T v7, T v8) noexcept
+    template<typename T2, int N_ELEM2>
+    operator SimdObject<T2, N_ELEM2>() { return data; }
+
+    template<typename T2>
+    SimdObject<T2, N_ELEM> convert_to()
     {
-        simd_set_all<T>(data, v1, v2, v3, v4);
+        SimdObject<T2, N_ELEM> result;
+        simd_convert<T2, T>( result.data, data );
+        return result;
     }
 
     template<int IDX> T get() const noexcept
     {
-        return simd_get_one<IDX, T>(data);
+        return simd_get_one<IDX, T>( data );
     }
 
-    template<int IDX> void set(T value) noexcept
+    template<int IDX> void set( T value ) noexcept
     {
-        simd_set_one<IDX, T>(data, value);
+        simd_set_one<IDX, T>( data, value );
     }
 
-    void set_all(T v1, T v2) noexcept
+    void set_all( T v1, T v2 ) noexcept
     {
-        simd_set_all<T>(data, v1, v2);
+        simd_set_all<T>( data, v1, v2 );
     }
 
-    void set_all(T v1, T v2, T v3, T v4) noexcept
+    void set_all( T v1, T v2, T v3, T v4 ) noexcept
     {
-        simd_set_all<T>(data, v1, v2, v3, v4);
+        simd_set_all<T>( data, v1, v2, v3, v4 );
     }
 
-    void set_all(T v1, T v2, T v3, T v4, T v5, T v6, T v7, T v8) noexcept
+    void set_all( T v1, T v2, T v3, T v4, T v5, T v6, T v7, T v8 ) noexcept
     {
-        simd_set_all<T>(data, v1, v2, v3, v4, v5, v6, v7, v8);
+        simd_set_all<T>( data, v1, v2, v3, v4, v5, v6, v7, v8 );
     }
 
-    void set_all(const T* values) noexcept
+    void set_all( const T* values ) noexcept
     {
-        simd_set_all<T>(data, values);
+        simd_set_all<T>( data, values );
     }
 
-    SimdObject& operator = (const SimdObject& peer) noexcept
+    void get_all( T* store ) noexcept
+    {
+        simd_get_all<T>( data, store );
+    }
+
+    SimdObject& operator = ( const SimdObject& peer ) noexcept
     {
         data = peer.data;
         return *this;
     }
 
-    SimdObject& operator = (const DataType& value) noexcept
+    SimdObject& operator = ( const DataType& value ) noexcept
     {
         data = value;
         return *this;
     }
 
-    SimdObject& operator = (T value) noexcept
+    SimdObject& operator = ( T value ) noexcept
     {
-        simd_broadcast(data, value);
+        simd_broadcast( data, value );
         return *this;
     }
 
-    SimdObject operator + (const SimdObject& peer) const noexcept
+    SimdObject operator + ( const SimdObject& peer ) const noexcept
     {
         SimdObject result;
-        simd_add<T>(result.data, data, peer.data);
+        simd_add<T>( result.data, data, peer.data );
         return result;
     }
 
-    SimdObject operator - (const SimdObject& peer) const noexcept
+    SimdObject operator - ( const SimdObject& peer ) const noexcept
     {
         SimdObject result;
-        simd_sub<T>(result.data, data, peer.data);
+        simd_sub<T>( result.data, data, peer.data );
         return result;
     }
 
-    SimdObject operator * (const SimdObject& peer) const noexcept
+    SimdObject operator * ( const SimdObject& peer ) const noexcept
     {
         SimdObject result;
-        simd_mul<T>(result.data, data, peer.data);
+        simd_mul<T>( result.data, data, peer.data );
         return result;
     }
 
-    SimdObject operator / (const SimdObject& peer) const noexcept
+    SimdObject operator / ( const SimdObject& peer ) const noexcept
     {
         SimdObject result;
-        simd_div<T>(result.data, data, peer.data);
-        return result;
-    }
-
-    template<typename T2, int N_ELEM2>
-    SimdObject operator & (const SimdObject<T2, N_ELEM2>& peer) const noexcept
-    {
-        SimdObject result;
-        simd_and<T>(result.data, data, peer.data);
+        simd_div<T>( result.data, data, peer.data );
         return result;
     }
 
     template<typename T2, int N_ELEM2>
-    SimdObject operator | (const SimdObject<T2, N_ELEM2>& peer) const noexcept
+    SimdObject operator & ( const SimdObject<T2, N_ELEM2>& peer ) const noexcept
     {
         SimdObject result;
-        simd_or<T>(result.data, data, peer.data);
+        simd_and<T>( result.data, data, peer.data );
         return result;
     }
 
     template<typename T2, int N_ELEM2>
-    SimdObject operator ^ (const SimdObject<T2, N_ELEM2>& peer) const noexcept
+    SimdObject operator | ( const SimdObject<T2, N_ELEM2>& peer ) const noexcept
     {
         SimdObject result;
-        simd_xor<T>(result.data, data, peer.data);
+        simd_or<T>( result.data, data, peer.data );
         return result;
     }
 
-    SimdObject& operator += (const SimdObject& peer) noexcept
+    template<typename T2, int N_ELEM2>
+    SimdObject operator ^ ( const SimdObject<T2, N_ELEM2>& peer ) const noexcept
     {
-        simd_add<T>(data, data, peer.data);
+        SimdObject result;
+        simd_xor<T>( result.data, data, peer.data );
+        return result;
+    }
+
+    SimdObject<T, N_ELEM> operator << ( int shift_num ) const noexcept
+    {
+        SimdObject result;
+        simd_shift_left( result.data, data, shift_num );
+        return result;
+    }
+
+    SimdObject<T, N_ELEM> operator >> ( int shift_num ) const noexcept
+    {
+        SimdObject result;
+        simd_shift_right_zero( result.data, data, shift_num );
+        return result;
+    }
+
+    SimdObject& operator += ( const SimdObject& peer ) noexcept
+    {
+        simd_add<T>( data, data, peer.data );
         return *this;
     }
 
-    SimdObject& operator -= (const SimdObject& peer) noexcept
+    SimdObject& operator -= ( const SimdObject& peer ) noexcept
     {
-        simd_sub<T>(data, data, peer.data);
+        simd_sub<T>( data, data, peer.data );
         return *this;
     }
 
-    SimdObject& operator *= (const SimdObject& peer) noexcept
+    SimdObject& operator *= ( const SimdObject& peer ) noexcept
     {
-        simd_mul<T>(data, data, peer.data);
+        simd_mul<T>( data, data, peer.data );
         return *this;
     }
 
-    SimdObject& operator /= (const SimdObject& peer) noexcept
+    SimdObject& operator /= ( const SimdObject& peer ) noexcept
     {
-        simd_div<T>(data, data, peer.data);
+        simd_div<T>( data, data, peer.data );
         return *this;
     }
 
     template<typename T2, int N_ELEM2>
-    SimdObject& operator &= (const SimdObject<T2, N_ELEM2>& peer) noexcept
+    SimdObject& operator &= ( const SimdObject<T2, N_ELEM2>& peer ) noexcept
     {
-        simd_and<T>(data, data, peer.data);
+        simd_and<T>( data, data, peer.data );
         return *this;
     }
 
     template<typename T2, int N_ELEM2>
-    SimdObject& operator |= (const SimdObject<T2, N_ELEM2>& peer) noexcept
+    SimdObject& operator |= ( const SimdObject<T2, N_ELEM2>& peer ) noexcept
     {
-        simd_or<T>(data, data, peer.data);
+        simd_or<T>( data, data, peer.data );
         return *this;
     }
 
     template<typename T2, int N_ELEM2>
-    SimdObject& operator ^= (const SimdObject<T2, N_ELEM2>& peer) noexcept
+    SimdObject& operator ^= ( const SimdObject<T2, N_ELEM2>& peer ) noexcept
     {
-        simd_xor<T>(data, data, peer.data);
+        simd_xor<T>( data, data, peer.data );
         return *this;
     }
 
     SimdObject operator ~ () const noexcept
     {
-        SimdObject result(data);
-        simd_cmpl<T>(result.data);
+        SimdObject result( data );
+        simd_cmpl<T>( result.data );
         return result;
     }
 
-    bool operator == (const SimdObject& peer) const noexcept
+    SimdObject<IntType, N_ELEM> operator == ( const SimdObject& peer ) const noexcept
     {
-        return simd_equal<T>(data, peer.data);
+        SimdObject<IntType, N_ELEM> result;
+        simd_cmp<T>( result.data, data, peer.data );
+        return result;
     }
 
-    bool operator != (const SimdObject& peer) const noexcept
+    SimdObject<IntType, N_ELEM> operator != ( const SimdObject& peer ) const noexcept
     {
-        return !simd_equal<T>(data, peer.data);
+        SimdObject<IntType, N_ELEM> result;
+        simd_ncmp<T>( result.data, data, peer.data );
+        return result;
+    }
+
+    SimdObject<IntType, N_ELEM> operator > ( const SimdObject& peer ) const noexcept
+    {
+        SimdObject<IntType, N_ELEM> result;
+        simd_gt<T>( result.data, data, peer.data );
+        return result;
+    }
+
+    SimdObject<IntType, N_ELEM> operator >= ( const SimdObject& peer ) const noexcept
+    {
+        SimdObject<IntType, N_ELEM> result;
+        simd_ge<T>( result.data, data, peer.data );
+        return result;
+    }
+
+    SimdObject<IntType, N_ELEM> operator < ( const SimdObject& peer ) const noexcept
+    {
+        SimdObject<IntType, N_ELEM> result;
+        simd_lt<T>( result.data, data, peer.data );
+        return result;
+    }
+
+    SimdObject<IntType, N_ELEM> operator <= ( const SimdObject& peer ) const noexcept
+    {
+        SimdObject<IntType, N_ELEM> result;
+        simd_le<T>( result.data, data, peer.data );
+        return result;
     }
 
     template<int IDX1, int IDX2, int IDX3, int IDX4>
     void shuffle() noexcept
     {
-        simd_shuffle<IDX1, IDX2, IDX3, IDX4>(data);
+        simd_shuffle<IDX1, IDX2, IDX3, IDX4>( data );
     }
 
     template<int IDX1, int IDX2, int IDX3, int IDX4>
     SimdObject get_shuffle() const noexcept
     {
-        SimdObject result(*this);
+        SimdObject result( *this );
         result.template shuffle<IDX1, IDX2, IDX3, IDX4>();
         return result;
     }
 
     T sum() const noexcept
     {
-        return simd_sum<T>(data);
+        return simd_sum<T>( data );
     }
 
     DataType data;
 };
+
+typedef SimdObject<float, 4> simd4f;
+typedef SimdObject<int32, 4> simd4i;
 
 } // namespace treecore
 
--- t/t_simd_obj_128.cpp
+++ t/t_simd_obj_128.cpp
@@ -1,6 +1,10 @@
 #include "treecore/TestFramework.h"
 #include "treecore/SimdObject.h"
 
+#include <limits>
+
+using treecore::int32;
+
 typedef treecore::SimdObject<float, 4> ftype;
 typedef treecore::SimdObject<treecore::int32, 4> itype;
 
@@ -8,179 +12,206 @@ void TestFramework::content( int argc, char** argv )
 {
     {
         ftype obj;
-        IS(obj.get<0>(), 0.0f);
-        IS(obj.get<1>(), 0.0f);
-        IS(obj.get<2>(), 0.0f);
-        IS(obj.get<3>(), 0.0f);
+        IS( obj.get<0>(),  0.0f );
+        IS( obj.get<1>(),  0.0f );
+        IS( obj.get<2>(),  0.0f );
+        IS( obj.get<3>(),  0.0f );
 
         obj = 1.1f;
-        IS(obj.get<0>(), 1.1f);
-        IS(obj.get<1>(), 1.1f);
-        IS(obj.get<2>(), 1.1f);
-        IS(obj.get<3>(), 1.1f);
+        IS( obj.get<0>(),  1.1f );
+        IS( obj.get<1>(),  1.1f );
+        IS( obj.get<2>(),  1.1f );
+        IS( obj.get<3>(),  1.1f );
 
-        ftype obj2(1.2f, 3.4f, 5.6f, 7.8f);
-        IS(obj2.get<0>(), 1.2f);
-        IS(obj2.get<1>(), 3.4f);
-        IS(obj2.get<2>(), 5.6f);
-        IS(obj2.get<3>(), 7.8f);
+        ftype obj2( 1.2f, 3.4f, 5.6f, 7.8f );
+        IS( obj2.get<0>(), 1.2f );
+        IS( obj2.get<1>(), 3.4f );
+        IS( obj2.get<2>(), 5.6f );
+        IS( obj2.get<3>(), 7.8f );
 
         obj = obj2;
-        IS(obj.get<0>(), 1.2f);
-        IS(obj.get<1>(), 3.4f);
-        IS(obj.get<2>(), 5.6f);
-        IS(obj.get<3>(), 7.8f);
+        IS( obj.get<0>(),  1.2f );
+        IS( obj.get<1>(),  3.4f );
+        IS( obj.get<2>(),  5.6f );
+        IS( obj.get<3>(),  7.8f );
 
-        obj.set_all(111.111f, 222.222f, 333.333f, 444.444f);
-        IS(obj.get<0>(), 111.111f);
-        IS(obj.get<1>(), 222.222f);
-        IS(obj.get<2>(), 333.333f);
-        IS(obj.get<3>(), 444.444f);
+        obj.set_all( 111.111f, 222.222f, 333.333f, 444.444f );
+        IS( obj.get<0>(), 111.111f );
+        IS( obj.get<1>(), 222.222f );
+        IS( obj.get<2>(), 333.333f );
+        IS( obj.get<3>(), 444.444f );
     }
 
-    OK("unary float arithmetic operations");
+    OK( "unary float arithmetic operations" );
     {
-        OK("add");
+        OK( "add" );
         ftype obj;
-        obj += ftype(234.5f, 678.9f, 111.1f, 123.4f);
-        IS(obj.get<0>(), 234.5f);
-        IS(obj.get<1>(), 678.9f);
-        IS(obj.get<2>(), 111.1f);
-        IS(obj.get<3>(), 123.4f);
+        obj += ftype( 234.5f, 678.9f, 111.1f, 123.4f );
+        IS( obj.get<0>(), 234.5f );
+        IS( obj.get<1>(), 678.9f );
+        IS( obj.get<2>(), 111.1f );
+        IS( obj.get<3>(), 123.4f );
 
-        OK("sub");
-        obj -= ftype(100.0f, 222.2f, 50.0f, 23.3f);
-        IS_EPSILON(obj.get<0>(), 134.5f);
-        IS_EPSILON(obj.get<1>(), 456.7f);
-        IS_EPSILON(obj.get<2>(), 61.1f);
-        IS_EPSILON(obj.get<3>(), 100.1f);
+        OK( "sub" );
+        obj -= ftype( 100.0f, 222.2f, 50.0f, 23.3f );
+        IS_EPSILON( obj.get<0>(), 134.5f );
+        IS_EPSILON( obj.get<1>(), 456.7f );
+        IS_EPSILON( obj.get<2>(),  61.1f );
+        IS_EPSILON( obj.get<3>(), 100.1f );
 
-        OK("mul");
-        obj *= ftype(1.2f, 3.4f, 5.6f, 7.8f);
-        IS_EPSILON(obj.get<0>(), 161.4f);
-        IS_EPSILON(obj.get<1>(), 1552.78f);
-        IS_EPSILON(obj.get<2>(), 342.16f);
-        IS_EPSILON(obj.get<3>(), 780.78f);
+        OK( "mul" );
+        obj *= ftype( 1.2f, 3.4f, 5.6f, 7.8f );
+        IS_EPSILON( obj.get<0>(),   161.4f );
+        IS_EPSILON( obj.get<1>(), 1552.78f );
+        IS_EPSILON( obj.get<2>(),  342.16f );
+        IS_EPSILON( obj.get<3>(),  780.78f );
 
-        OK("div");
-        obj /= ftype(2.0f, 3.0f, 5.0f, 7.0f);
-        IS_EPSILON(obj.get<0>(), 80.7f);
-        IS_EPSILON(obj.get<1>(), 517.5933f);
-        IS_EPSILON(obj.get<2>(), 68.432f);
-        IS_EPSILON(obj.get<3>(), 111.54f);
+        OK( "div" );
+        obj /= ftype( 2.0f, 3.0f, 5.0f, 7.0f );
+        IS_EPSILON( obj.get<0>(),     80.7f );
+        IS_EPSILON( obj.get<1>(), 517.5933f );
+        IS_EPSILON( obj.get<2>(),   68.432f );
+        IS_EPSILON( obj.get<3>(),   111.54f );
     }
 
-    OK("unary logic operations");
+    OK( "unary logic operations" );
     {
-        OK("and");
-        itype obj(0xffff0000, 0xf0f0f0f0, 0xff00ff00, 0x0ff00ff0);
-        obj &= itype(0x000ff000, 0xff00ff00, 0x0ff00ff0, 0xffff0000);
-        IS(obj.get<0>(), 0x000f0000);
-        IS(obj.get<1>(), 0xf000f000);
-        IS(obj.get<2>(), 0x0f000f00);
-        IS(obj.get<3>(), 0x0ff00000);
+        OK( "and" );
+        itype obj( 0xffff0000, 0xf0f0f0f0, 0xff00ff00, 0x0ff00ff0 );
+        obj &= itype( 0x000ff000, 0xff00ff00, 0x0ff00ff0, 0xffff0000 );
+        IS( obj.get<0>(), 0x000f0000 );
+        IS( obj.get<1>(), 0xf000f000 );
+        IS( obj.get<2>(), 0x0f000f00 );
+        IS( obj.get<3>(), 0x0ff00000 );
 
-        OK("or");
-        obj |= itype(0x000fff00, 0x0ff00000, 0xff00000f, 0x0000ff00);
-        IS(obj.get<0>(), 0x000fff00);
-        IS(obj.get<1>(), 0xfff0f000);
-        IS(obj.get<2>(), 0xff000f0f);
-        IS(obj.get<3>(), 0x0ff0ff00);
+        OK( "or" );
+        obj |= itype( 0x000fff00, 0x0ff00000, 0xff00000f, 0x0000ff00 );
+        IS( obj.get<0>(), 0x000fff00 );
+        IS( obj.get<1>(), 0xfff0f000 );
+        IS( obj.get<2>(), 0xff000f0f );
+        IS( obj.get<3>(), 0x0ff0ff00 );
 
-        OK("xor");
-        obj ^= itype(0x00ff00ff, 0xffff0000, 0x0000ffff, 0xf0f0f0f0);
-        IS(obj.get<0>(), 0x00f0ffff);
-        IS(obj.get<1>(), 0x000ff000);
-        IS(obj.get<2>(), 0xff00f0f0);
-        IS(obj.get<3>(), 0xff000ff0);
+        OK( "xor" );
+        obj ^= itype( 0x00ff00ff, 0xffff0000, 0x0000ffff, 0xf0f0f0f0 );
+        IS( obj.get<0>(), 0x00f0ffff );
+        IS( obj.get<1>(), 0x000ff000 );
+        IS( obj.get<2>(), 0xff00f0f0 );
+        IS( obj.get<3>(), 0xff000ff0 );
 
-        OK("complement");
+        OK( "complement" );
         itype result = ~obj;
-        IS(result.get<0>(), 0xff0f0000);
-        IS(result.get<1>(), 0xfff00fff);
-        IS(result.get<2>(), 0x00ff0f0f);
-        IS(result.get<3>(), 0x00fff00f);
+        IS( result.get<0>(), 0xff0f0000 );
+        IS( result.get<1>(), 0xfff00fff );
+        IS( result.get<2>(), 0x00ff0f0f );
+        IS( result.get<3>(), 0x00fff00f );
     }
 
-    OK("binary float arithmetic operators");
+    OK( "binary float arithmetic operators" );
     {
-        OK("add");
-        ftype result = ftype(1.2f, 3.4f, 5.6f, 7.8f) + ftype(1.1f, 2.2f, 3.3f, 4.4f);
-        IS_EPSILON(result.get<0>(), 2.3f);
-        IS_EPSILON(result.get<1>(), 5.6f);
-        IS_EPSILON(result.get<2>(), 8.9f);
-        IS_EPSILON(result.get<3>(), 12.2f);
+        OK( "add" );
+        ftype result = ftype( 1.2f, 3.4f, 5.6f, 7.8f ) + ftype( 1.1f, 2.2f, 3.3f, 4.4f );
+        IS_EPSILON( result.get<0>(),  2.3f );
+        IS_EPSILON( result.get<1>(),  5.6f );
+        IS_EPSILON( result.get<2>(),  8.9f );
+        IS_EPSILON( result.get<3>(), 12.2f );
     }
     {
-        OK("sub");
-        ftype result = ftype(555.444f, 222.333f, 111.111f, 345.123f) - ftype(666.666f, 555.555f, 444.444f, 333.333f);
-        IS_EPSILON(result.get<0>(), -111.222f);
-        IS_EPSILON(result.get<1>(), -333.222f);
-        IS_EPSILON(result.get<2>(), -333.333f);
-        IS_EPSILON(result.get<3>(), 11.79f);
+        OK( "sub" );
+        ftype result = ftype( 555.444f, 222.333f, 111.111f, 345.123f ) - ftype( 666.666f, 555.555f, 444.444f, 333.333f );
+        IS_EPSILON( result.get<0>(),      -111.222f );
+        IS_EPSILON( result.get<1>(),      -333.222f );
+        IS_EPSILON( result.get<2>(),      -333.333f );
+        IS_EPSILON( result.get<3>(), 11.79f );
     }
     {
-        OK("mul");
-        ftype result = ftype(1.2f, 3.4f, 5.6f, 7.8f) * ftype(1.1f, 2.2f, 3.3f, 4.4f);
-        IS_EPSILON(result.get<0>(), 1.32f);
-        IS_EPSILON(result.get<1>(), 7.48f);
-        IS_EPSILON(result.get<2>(), 18.48f);
-        IS_EPSILON(result.get<3>(), 34.32f);
+        OK( "mul" );
+        ftype result = ftype( 1.2f, 3.4f, 5.6f, 7.8f ) * ftype( 1.1f, 2.2f, 3.3f, 4.4f );
+        IS_EPSILON( result.get<0>(),  1.32f );
+        IS_EPSILON( result.get<1>(),  7.48f );
+        IS_EPSILON( result.get<2>(), 18.48f );
+        IS_EPSILON( result.get<3>(), 34.32f );
     }
     {
-        OK("div");
-        ftype result = ftype(1.2f, 3.4f, 5.6f, 7.8f) / ftype(1.1f, 2.2f, 3.3f, 4.4f);
-        IS_EPSILON(result.get<0>(), 1.090909f);
-        IS_EPSILON(result.get<1>(), 1.545455f);
-        IS_EPSILON(result.get<2>(), 1.696970f);
-        IS_EPSILON(result.get<3>(), 1.772727f);
+        OK( "div" );
+        ftype result = ftype( 1.2f, 3.4f, 5.6f, 7.8f ) / ftype( 1.1f, 2.2f, 3.3f, 4.4f );
+        IS_EPSILON( result.get<0>(), 1.090909f );
+        IS_EPSILON( result.get<1>(), 1.545455f );
+        IS_EPSILON( result.get<2>(), 1.696970f );
+        IS_EPSILON( result.get<3>(), 1.772727f );
     }
 
-    OK("binary logic operations");
+    OK( "binary logic operations" );
     {
         itype result;
 
-        OK("and");
-        result = itype(0xffff0000, 0xf0f0f0f0, 0xff00ff00, 0x0ff00ff0) & itype(0x000ff000, 0xff00ff00, 0x0ff00ff0, 0xffff0000);
-        IS(result.get<0>(), 0x000f0000);
-        IS(result.get<1>(), 0xf000f000);
-        IS(result.get<2>(), 0x0f000f00);
-        IS(result.get<3>(), 0x0ff00000);
+        OK( "and" );
+        result = itype( 0xffff0000, 0xf0f0f0f0, 0xff00ff00, 0x0ff00ff0 ) & itype( 0x000ff000, 0xff00ff00, 0x0ff00ff0, 0xffff0000 );
+        IS( result.get<0>(), 0x000f0000 );
+        IS( result.get<1>(), 0xf000f000 );
+        IS( result.get<2>(), 0x0f000f00 );
+        IS( result.get<3>(), 0x0ff00000 );
 
-        OK("or");
-        result = itype(0x000f0000, 0xf000f000, 0x0f000f00, 0x0ff00000) | itype(0x000fff00, 0x0ff00000, 0xff00000f, 0x0000ff00);
-        IS(result.get<0>(), 0x000fff00);
-        IS(result.get<1>(), 0xfff0f000);
-        IS(result.get<2>(), 0xff000f0f);
-        IS(result.get<3>(), 0x0ff0ff00);
+        OK( "or" );
+        result = itype( 0x000f0000, 0xf000f000, 0x0f000f00, 0x0ff00000 ) | itype( 0x000fff00, 0x0ff00000, 0xff00000f, 0x0000ff00 );
+        IS( result.get<0>(), 0x000fff00 );
+        IS( result.get<1>(), 0xfff0f000 );
+        IS( result.get<2>(), 0xff000f0f );
+        IS( result.get<3>(), 0x0ff0ff00 );
 
-        OK("xor");
-        result = itype(0x000fff00, 0xfff0f000, 0xff000f0f, 0x0ff0ff00) ^ itype(0x00ff00ff, 0xffff0000, 0x0000ffff, 0xf0f0f0f0);
-        IS(result.get<0>(), 0x00f0ffff);
-        IS(result.get<1>(), 0x000ff000);
-        IS(result.get<2>(), 0xff00f0f0);
-        IS(result.get<3>(), 0xff000ff0);
+        OK( "xor" );
+        result = itype( 0x000fff00, 0xfff0f000, 0xff000f0f, 0x0ff0ff00 ) ^ itype( 0x00ff00ff, 0xffff0000, 0x0000ffff, 0xf0f0f0f0 );
+        IS( result.get<0>(), 0x00f0ffff );
+        IS( result.get<1>(), 0x000ff000 );
+        IS( result.get<2>(), 0xff00f0f0 );
+        IS( result.get<3>(), 0xff000ff0 );
     }
 
-    OK("masking of float and int");
+    OK( "masking of float and int" );
     {
         ftype result;
-        result = ftype(111.222f, 333.444f, 555.666f, 777.888f) & itype(0xffffffff, 0x00000000, 0xffffffff, 0x00000000);
-        IS(result.get<0>(), 111.222f);
-        IS(result.get<1>(), 0.0f);
-        IS(result.get<2>(), 555.666f);
-        IS(result.get<3>(), 0.0f);
+        result = ftype( 111.222f, 333.444f, 555.666f, 777.888f ) & itype( 0xffffffff, 0x00000000, 0xffffffff, 0x00000000 );
+        IS( result.get<0>(), 111.222f );
+        IS( result.get<1>(),     0.0f );
+        IS( result.get<2>(), 555.666f );
+        IS( result.get<3>(),     0.0f );
     }
 
-    OK("equality test");
+    OK( "float comparison" );
     {
-        OK(ftype(111.222f, 333.444f, 555.666f, 777.888f) == ftype(111.222f, 333.444f, 555.666f, 777.888f));
-        OK(ftype(111.222f, 333.444f, 555.666f, 777.888f) != ftype(111.222f, 333.444f, 555.666f, 777.898f));
-        OK(ftype(111.222f, 333.444f, 555.666f, 777.888f) != ftype(111.222f, 333.444f, 555.626f, 777.888f));
-        OK(ftype(111.222f, 333.444f, 555.666f, 777.888f) != ftype(111.222f, 333.144f, 555.666f, 777.888f));
-        OK(ftype(111.222f, 333.444f, 555.666f, 777.888f) != ftype(114.222f, 333.444f, 555.666f, 777.888f));
+        ftype value1( 111.222f, 333.144f, std::numeric_limits<float>::infinity(), 777.898f );
+        ftype value2( 111.222f, 333.444f, 555.666f, 777.888f );
+        itype result;
+
+        result = (value1 == value2);
+        IS( result.get<0>(), int32( 0xffffffff ) );
+        IS( result.get<1>(), int32( 0 ) );
+        IS( result.get<2>(), int32( 0 ) );
+        IS( result.get<3>(), int32( 0 ) );
+
+        result = (value1 > value2);
+        IS( result.get<0>(), int32( 0 ) );
+        IS( result.get<1>(), int32( 0 ) );
+        IS( result.get<2>(), int32( 0xffffffff ) );
+        IS( result.get<3>(), int32( 0xffffffff ) );
+
+        result = (value1 >= value2);
+        IS( result.get<0>(), int32( 0xffffffff ) );
+        IS( result.get<1>(), int32( 0 ) );
+        IS( result.get<2>(), int32( 0xffffffff ) );
+        IS( result.get<3>(), int32( 0xffffffff ) );
+
+        result = (value1 < value2);
+        IS( result.get<0>(), int32( 0 ) );
+        IS( result.get<1>(), int32( 0xffffffff ) );
+        IS( result.get<2>(), int32( 0 ) );
+        IS( result.get<3>(), int32( 0 ) );
+
+        result = (value1 <= value2);
+        IS( result.get<0>(), int32( 0xffffffff ) );
+        IS( result.get<1>(), int32( 0xffffffff ) );
+        IS( result.get<2>(), int32( 0 ) );
+        IS( result.get<3>(), int32( 0 ) );
     }
 
 }
-
 
